4465237872e686474e680184107dbbc1e2e9c324
MarcelaBezerra <136825587+MarcelaBezerra@users.noreply.github.com>
2023-06-17 15:05:10 -0300

    Delete agendamentos directory

--- agendamentos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Agendamentos
-
-# Register your models here.
-@admin.register(Agendamentos)
-class AgedamentosAdmin(admin.ModelAdmin):
-    list_display = ('age_codigo',)
