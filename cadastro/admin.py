0f73dc33d7eae0999a082b76d31867533d8799f3
MarcelaBezerra <136825587+MarcelaBezerra@users.noreply.github.com>
2023-06-17 15:05:24 -0300

    Delete cadastro directory

--- cadastro/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Cadastro
-
-@admin.register(Cadastro)
-class CadastroAdmin(admin.ModelAdmin):
-    list_display = ('cad_nome', 'cad_email', 'cad_rua', 'cad_numero', 'cad_bairro', 'cad_cidade', 'cad_contato')
-    list_filter = ('cad_nome', 'cad_email', 'cad_rua', 'cad_numero', 'cad_bairro', 'cad_cidade', 'cad_contato')
-    list_editable = ('cad_email', 'cad_rua', 'cad_numero', 'cad_bairro', 'cad_cidade', 'cad_contato')
-    search_fields = ('cad_nome', 'cad_email', 'cad_rua', 'cad_numero', 'cad_bairro', 'cad_cidade', 'cad_contato')
--- cadastro/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.cadastro, name='cadastro'),
-    path('processa_cadastro/', views.processa_cadastro, name='processa_cadastro')
-]
