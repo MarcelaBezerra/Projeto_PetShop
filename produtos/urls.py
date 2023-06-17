d463e376548eb7917997890e739c7dfa222f9988
MarcelaBezerra <136825587+MarcelaBezerra@users.noreply.github.com>
2023-06-17 15:06:09 -0300

    Delete produtos directory

--- produtos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.produtos, name='produtos'),
-    path('categoria/', views.categorias, name='categorias'),
-    path('compra/', views.compra, name='compra')
-]
